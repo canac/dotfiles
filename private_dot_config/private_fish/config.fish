47b9af7a87c8e97bec879cbc86f9b87f3a9242d1
Caleb Cox <canac@users.noreply.github.com>
2021-07-30 11:50:25 -0500

    Install Ruby

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,2 @@
-set GEM_HOME $HOME/.gem
-set PATH /Users/caleb/.kenv/bin /Users/caleb/.local/bin $HOME/.gem/ruby/2.6.0/bin /usr/local/sbin $PATH
+set PATH /Users/caleb/.kenv/bin /Users/caleb/.local/bin /usr/local/opt/ruby/bin /usr/local/sbin $PATH
 sh "$HOME/.cargo/env"

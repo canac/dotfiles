cbc9f1ab9818e27cf3638135eed2381293db5825
Caleb Cox <canac@users.noreply.github.com>
2021-08-12 09:53:32 -0500

    Update journal scripts

---
+++ dot_kenv/scripts/journal-tags.js
@@ -0,0 +1,36 @@
+// Menu: Journal tags
+// Description: Choose journal tags
+
+import { readdir, readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+const { sortBy } = await npm('lodash-es');
+
+const postsDir = '/Users/caleb/dev/journal/jekyll/_posts';
+const posts = await readdir(postsDir);
+const usedTags = new Set();
+for (const filename of posts) {
+  const post = await readFile(join(postsDir, filename), 'utf8');
+  const matches = /---\n[\s\S]*tags: ?(.*)[\s\S]*\n---\n/g.exec(post);
+  if (!matches) {
+    console.log(`Could not extract tags from ${filename}`);
+  }
+
+  const tags = matches[1];
+  if (!tags) {
+    // The post had no tags
+    continue;
+  }
+
+  tags.split(' ').forEach(tag => {
+    usedTags.add(tag);
+  });
+}
+
+// Sort alphabetically, ignoring case
+const sortedTags = sortBy(Array.from(usedTags.keys()).sort(), tag => tag.toLowerCase());
+
+// Loop to allow picking multiple tags
+while (true) {
+  const chosenTag = await arg('Journal tag:', sortedTags);
+  await setSelectedText(`${chosenTag} `);
+}
--- dot_kenv/scripts/new-journal.js
+++ dot_kenv/scripts/new-journal.js
@@ -6,37 +6,59 @@ import { join } from 'node:path';
 const { addDays, format } = await npm('date-fns');
 const { findLast, mapValues } = await npm('lodash');
 
-const pattern = /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})-(?<index>\d+)\.md$/;
+const pattern =
+  /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})-(?<index>\d+)\.md$/;
 
 const postsDir = '/Users/caleb/dev/journal/jekyll/_posts';
-const lastPost = findLast(await readdir(postsDir), file => pattern.test(file));
-const { year, month, day, index } = mapValues(pattern.exec(lastPost).groups, str => parseInt(str, 10));
+const lastPost = findLast(await readdir(postsDir), (file) =>
+  pattern.test(file),
+);
+const { year, month, day, index } = mapValues(
+  pattern.exec(lastPost).groups,
+  (str) => parseInt(str, 10),
+);
 const lastDay = new Date(year, month - 1, day);
 const nextDay = addDays(lastDay, 1);
-const newPost = await arg('Post date:', [{
-  value: {
-    date: nextDay,
-    index: 1,
+const newPost = await arg('Post date:', [
+  {
+    value: {
+      date: nextDay,
+      index: 1,
+    },
+    name: 'Next day',
+    description: `Journal about the next day (${format(
+      nextDay,
+      'EEEE, MMMM d',
+    )})`,
   },
-  name: 'Next day',
-  description: `Journal about the next day (${format(nextDay, 'EEEE, MMMM d')})`,
-}, {
-  value: {
-    date: lastDay,
-    index: index + 1,
+  {
+    value: {
+      date: lastDay,
+      index: index + 1,
+    },
+    name: 'Same day',
+    description: `Journal about the last day again (${format(
+      lastDay,
+      'EEEE, MMMM d',
+    )})`,
   },
-  name: 'Same day',
-  description: `Journal about the last day again (${format(lastDay, 'EEEE, MMMM d')})`,
-}]);
+]);
 
 const numberFormattedDate = format(new Date(newPost.date), 'yyyy-MM-dd');
 const wordFormattedDate = format(new Date(newPost.date), 'EEEE, MMMM d, y');
-const newFilename = join(postsDir, `${numberFormattedDate}-${newPost.index}.md`);
-await writeFile(newFilename, `---
-title: "${wordFormattedDate} #${newPost.index}"
+const newFilename = join(
+  postsDir,
+  `${numberFormattedDate}-${newPost.index}.md`,
+);
+await writeFile(
+  newFilename,
+  `---
+title: '${wordFormattedDate} #${newPost.index}'
 date: ${numberFormattedDate}
-tags: ` + `
+tags: ` +
+    `
 ---
 
-Jesus, `);
+Jesus, `,
+);
 edit(`${newFilename}`, null, 4, 7);

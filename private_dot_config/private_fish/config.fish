c4f4f45e91c933b25a4b698951523c5247e7ab47
Caleb Cox <canac@users.noreply.github.com>
2021-09-10 22:03:36 -0500

    Use $HOME

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,2 +1,2 @@
-set PATH /Users/caleb/.kenv/bin /Users/caleb/.local/bin /usr/local/opt/ruby/bin /usr/local/sbin $PATH
+set PATH $HOME/.kenv/bin $HOME/.local/bin /usr/local/opt/ruby/bin /usr/local/sbin $PATH
 sh "$HOME/.cargo/env"

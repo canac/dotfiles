965456fa338b25f56dd54823235fd6d0fc723f74
Caleb Cox <canac@users.noreply.github.com>
2021-09-11 20:18:05 -0500

    Add go binaries to PATH

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,3 @@
-set PATH $HOME/.kenv/bin $HOME/.local/bin /usr/local/opt/ruby/bin /usr/local/sbin $PATH
+set PATH $HOME/.kenv/bin $HOME/.local/bin /usr/local/opt/ruby/bin (go env GOPATH)/bin /usr/local/sbin $PATH
 sh "$HOME/.cargo/env"
 starship init fish | source

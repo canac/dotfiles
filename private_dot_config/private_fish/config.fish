f75bf65e34921de5223c7d83a623a6a940a20a25
Caleb Cox <canac@users.noreply.github.com>
2021-09-13 16:17:20 -0500

    Add iTerm shell integration

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,2 +1,3 @@
 set PATH $HOME/.kenv/bin $HOME/.local/bin /usr/local/opt/ruby/bin (go env GOPATH)/bin $HOME/.cargo/bin /usr/local/sbin $PATH
 starship init fish | source
+source $HOME/.iterm2_shell_integration.fish

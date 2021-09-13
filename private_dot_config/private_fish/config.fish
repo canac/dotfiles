8d892214a549b3f44bdadbbb458eecc490dad50a
Caleb Cox <canac@users.noreply.github.com>
2021-09-13 18:35:32 -0500

    Set paths with fish_add_path

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,2 @@
-set PATH $HOME/.kenv/bin $HOME/.local/bin /usr/local/opt/ruby/bin (go env GOPATH)/bin $HOME/.cargo/bin /usr/local/sbin $PATH
 starship init fish | source
 source $HOME/.iterm2_shell_integration.fish

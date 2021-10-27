73fb8576c5de060b356c7634ffb16870c481fdc7
Caleb Cox <canac@users.noreply.github.com>
2021-10-27 09:11:20 -0500

    Fix typo

--- private_dot_config/private_fish/functions/mkdir_and_cd.fish
+++ private_dot_config/private_fish/functions/mkdir_and_cd.fish
@@ -1,4 +1,4 @@
-function mkdir_and_cd --description 'mkdir a new direction and cd into it'
+function mkdir_and_cd --description 'mkdir a new directory and cd into it'
     mkdir -p "$argv"
     cd "$argv"
 end

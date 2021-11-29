c9cfd19c8606ffa4d7fd2fd403eeada286d042d9
Caleb Cox <canac@users.noreply.github.com>
2021-11-29 13:33:41 -0600

    Add nextdns

---
+++ .chezmoiscripts/run_once_before_phase2-setup-packages.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env npx zx
+
+// In phase 2, use zx, which was installed in phase 1, to configure installed packages because it
+// is a nicer scripting language than bash
+
+// Configure fzf
+await $`$(brew --prefix)/opt/fzf/install --key-bindings --no-completion --no-update-rc --no-bash --no-zsh`;
+
+// Configure nextdns
+if ((await $`sudo nextdns status`) !== 'running') {
+  await $`sudo nextdns install --config 1d7724 -report-client-info -auto-activate`;
+}
+
+// Change the shell to fish
+const shellsPath = '/etc/shells';
+const fish = (await $`which fish`).stdout.trim();
+if ((await nothrow($`grep ${fish} ${shellsPath}`).exitCode) !== 0) {
+  await $`echo ${fish} | sudo tee -a ${shellsPath}`;
+}
+
+if (process.env.SHELL !== fish) {
+  await $`chsh -s ${fish}`;
+}

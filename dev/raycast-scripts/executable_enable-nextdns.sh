b3cf346a839055e29d6655986a4a266fb919b928
Caleb Cox <canac@users.noreply.github.com>
2021-12-08 11:48:27 -0600

    Add Raycast scripts

---
+++ dev/raycast-scripts/executable_enable-nextdns.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Enable NextDNS
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Start using NextDNS for DNS resolution on this machine
+# @raycast.author Caleb Cox
+# @raycast.authorURL https://github.com/canac
+
+nextdns activate

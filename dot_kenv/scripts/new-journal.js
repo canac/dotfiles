62272fcafc02511099c44aaa77437fc24383971d
Caleb Cox <canac@users.noreply.github.com>
2022-01-07 09:14:18 -0600

    Support skipping journal days

--- dot_kenv/scripts/new-journal.js
+++ dot_kenv/scripts/new-journal.js
@@ -6,18 +6,21 @@ import '@johnlindquist/kit';
 import { readdir, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 const { addDays, format } = await npm('date-fns');
-const { findLast, mapValues } = await npm('lodash-es');
+const { findLast, mapValues, range } = await npm('lodash-es');
 
-const pattern =
+function formatDateHuman(date) {
+  return format(date, 'EEEE, MMMM d');
+}
+
+const postFilenamePattern =
   /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})-(?<index>\d+)\.md$/;
-
 const journalDir = '/Users/caleb/dev/journal';
 const postsDir = join(journalDir, 'jekyll', '_posts');
 const lastPost = findLast(await readdir(postsDir), (file) =>
-  pattern.test(file),
+  postFilenamePattern.test(file),
 );
 const { year, month, day, index } = mapValues(
-  pattern.exec(lastPost).groups,
+  postFilenamePattern.exec(lastPost).groups,
   (str) => parseInt(str, 10),
 );
 const lastDay = new Date(year, month - 1, day);
@@ -29,10 +32,7 @@ const newPost = await arg('Post date:', [
       index: 1,
     },
     name: 'Next day',
-    description: `Journal about the next day (${format(
-      nextDay,
-      'EEEE, MMMM d',
-    )})`,
+    description: `Journal about the next day (${formatDateHuman(nextDay)})`,
   },
   {
     value: {
@@ -40,11 +40,21 @@ const newPost = await arg('Post date:', [
       index: index + 1,
     },
     name: 'Same day',
-    description: `Journal about the last day again (${format(
+    description: `Journal about the last day again (${formatDateHuman(
       lastDay,
-      'EEEE, MMMM d',
     )})`,
   },
+  ...range(7).map((skip) => {
+    const date = addDays(nextDay, skip + 1);
+    return {
+      value: {
+        date,
+        index: 1,
+      },
+      name: `Skip ${skip + 1} day${skip === 0 ? '' : 's'}`,
+      description: `Journal about ${formatDateHuman(date)}`,
+    };
+  }),
 ]);
 
 const numberFormattedDate = format(new Date(newPost.date), 'yyyy-MM-dd');

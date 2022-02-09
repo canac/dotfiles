69799d7a3c00e2995241c2c6eacc9e8beda17a7b
Caleb Cox <canac@users.noreply.github.com>
2022-02-09 16:14:31 -0600

    Add online backup and Homebrew update Raycast scripts

--- dot_kenv/scripts/online-backup.js
+++
@@ -1,8 +0,0 @@
-// Menu: Online backup
-// Description: Begin new online backup snapshot using Restic
-// Schedule: 0 */3 * * *
-// Runs every three hours
-
-import '@johnlindquist/kit';
-
-await exec('fish -c backup');

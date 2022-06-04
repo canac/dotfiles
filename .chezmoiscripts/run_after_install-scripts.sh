89a30867ed6776933f72c9b67cff9a9fc87d9cf8
Caleb Cox <canac@users.noreply.github.com>
2022-06-04 13:51:54 -0500

    Use Deno-based chron

--- .chezmoiscripts/run_after_install-scripts.sh
+++ .chezmoiscripts/run_after_install-scripts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Install all user scripts
-cargo install --git https://github.com/canac/chron.git
+deno install --allow-env=HOME,PORT --allow-net=0.0.0.0:$PORT --allow-run --allow-read=$HOME/.local/share/chron,$HOME/.config/chron/chronfile.toml --allow-write=$HOME/.local/share/chron --name chron --force https://denopkg.com/canac/chron-deno@0.1.0/main.ts
 deno install --allow-env=CHRON_MAILBOX_URL,HOME --allow-net -f ~/dev/scripts/release-watcher.ts
+
+fish -c 'lcman restart chron'

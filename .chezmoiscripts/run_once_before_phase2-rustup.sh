77298deef40326880aba31eebf17fbed14165dfb
Caleb Cox <canac@users.noreply.github.com>
2022-11-01 20:24:35 -0500

    Install clippy

--- .chezmoiscripts/run_once_before_phase2-rustup.sh
+++ .chezmoiscripts/run_once_before_phase2-rustup.sh
@@ -2,3 +2,7 @@
 
 # Run rustup-init if it hasn't been run before
 test -e "$HOME/.rustup" || rustup-init -y --no-modify-path
+
+# Install clippy if it isn't installed
+rustup_bin=$HOME/.cargo/bin/rustup
+$rustup_bin component list --installed | grep clippy || $rustup_bin component add clippy

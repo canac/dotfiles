311fe00583c8cd33a2a3ca2bd4bf3d40fced91fd
Caleb Cox <canac@users.noreply.github.com>
2023-01-19 16:40:33 -0600

    Add exponential star notification decay

--- dev/scripts/stargazer.ts
+++ dev/scripts/stargazer.ts
@@ -82,7 +82,7 @@ async function* loadRepos(criteria: string): AsyncIterable<Repo> {
     const res = await fetch("https://api.github.com/graphql", {
       method: "POST",
       headers: {
-        authorization: `bearer ${token}`,
+        authorization: `Bearer ${token}`,
       },
       body,
     });
@@ -107,7 +107,7 @@ async function getNewRepos(
   threshold: number,
 ): Promise<Repo[]> {
   const repos = [];
-  for await (const repo of loadRepos(criteria)) {
+  for await (const repo of loadRepos(`stars:>=${threshold} ${criteria}`)) {
     repos.push(repo);
   }
 
@@ -117,17 +117,23 @@ async function getNewRepos(
     return [];
   }
 
-  // Get new repos and old repos that have increased to the next `threshold` multiple of stars
+  // Get new repos and old repos that have increased to the whole-number exponent `threshold` of stars
   const newRepos = new Set(
     db.query(
       `WITH new_repos(name, stars) AS (VALUES ${
         repos.map(() => "(?, ?)").join(",")
       })
       INSERT INTO seen_repos (name, stars)
-      SELECT new_repos.name, new_repos.stars FROM new_repos LEFT JOIN seen_repos USING(name) WHERE seen_repos.stars IS NULL OR new_repos.stars - new_repos.stars % ? > seen_repos.stars
+      SELECT new_repos.name, new_repos.stars FROM new_repos LEFT JOIN seen_repos USING(name) WHERE seen_repos.stars IS NULL OR new_repos.stars > seen_repos.stars
       ON CONFLICT(name) DO UPDATE SET stars=excluded.stars
       RETURNING name, stars`,
-      [...repos.flatMap((repo) => [repo.name, repo.stars]), threshold],
+      repos.flatMap((
+        repo,
+      ) => [
+        repo.name,
+        // Truncate to a whole-number exponent of `threshold`
+        Math.pow(2, Math.floor(Math.log2(repo.stars / threshold))) * threshold,
+      ]),
     ).map(([name]) => name as string),
   );
   return repos.filter((repo) => newRepos.has(repo.name));

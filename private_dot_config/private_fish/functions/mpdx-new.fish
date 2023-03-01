c198f94dd5a8f9127a35b4f942aadf36d22839dc
Caleb Cox <canac@users.noreply.github.com>
2023-03-01 15:45:51 -0600

    Update mpdx-new

--- private_dot_config/private_fish/functions/mpdx-new.fish
+++ private_dot_config/private_fish/functions/mpdx-new.fish
@@ -1,13 +1,15 @@
 function mpdx-new --description 'Create a new MPDX worktree'
-    set jira $(gum input --placeholder "Jira issue number")
-    set branch $(gum input --placeholder "Git branch suffix")
-    set directory "$HOME/dev/mpdx-$jira"
-    set root_directory "$HOME/dev/mpdx-react"
-    cd $root_directory
-    git worktree add -b mpdx-$jira-$branch $directory main
-    cd $directory
-    cp $root_directory/.env $directory
-    yarn
-    yarn gql
-    yarn gql:server
+    set dirname_suffix $(gum input --prompt "Directory name: mpdx-" --placeholder="") &&
+    set dirname "mpdx-$dirname_suffix" &&
+    set directory "$HOME/dev/$dirname" &&
+    set branch $(gum input --prompt "Branch name: " --value "$dirname_suffix") &&
+    set root_directory "$HOME/dev/mpdx-react" &&
+    cd $root_directory &&
+    git worktree add -b $branch $directory main &&
+    cd $directory &&
+    cp $root_directory/.env $directory &&
+    yarn &&
+    yarn gql &&
+    yarn gql:server &&
+    code .
 end

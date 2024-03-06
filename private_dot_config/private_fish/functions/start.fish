7b901bbe85c4247e89b4e5d2bc781a2094951401
Caleb Cox <canac@users.noreply.github.com>
2024-03-06 08:17:58 -0600

    Upgrade portman to v0.6

--- private_dot_config/private_fish/functions/start.fish
+++ private_dot_config/private_fish/functions/start.fish
@@ -1,25 +1,10 @@
 function start --description 'Start a project'
-    set repo $(git remote get-url origin)
-    if test $repo = "https://github.com/CruGlobal/conf-registration-web.git"
-        portman create --overwrite --link 9000
+    portman link
+    if test -e yarn.lock
         yarn start
-    else if test $repo = "https://github.com/CruGlobal/give-web.git"
-        portman create --overwrite --link 9000
-        yarn start
-    else if test $repo = "https://github.com/CruGlobal/ml.git"
-        portman create --overwrite --link 9000
-        yarn start
-    else if test $repo = "https://github.com/CruGlobal/mpdx_api.git"
-        portman create --overwrite --link 3001
-        rails s
-    else if test $repo = "https://github.com/CruGlobal/mpdx-react.git"
-        portman create --overwrite --link 3000
-        yarn start
-    else if test $repo = "https://github.com/CruGlobal/mpdx_web.git"
-        portman create --overwrite --link 8081
+    else if test -e package-lock.json
         npm start
     else
-        echo "Warning: unrecognized repository $repo"
-        yarn start
+        echo "No yarn.lock or package-lock.json file found"
     end
 end

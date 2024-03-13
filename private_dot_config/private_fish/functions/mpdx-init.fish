a36c2e07392c50230910f16a3916184678e634de
Caleb Cox <canac@users.noreply.github.com>
2024-03-13 11:58:30 -0500

    Add worktree-add function

--- private_dot_config/private_fish/functions/mpdx-init.fish
+++
@@ -1,6 +0,0 @@
-function mpdx-init --description 'Initialize a new MPDX worktree'
-    cp $HOME/dev/mpdx-react/.env .
-    yarn
-    yarn gql
-    yarn gql:server
-end
---
+++ private_dot_config/private_fish/functions/worktree-add.fish
@@ -0,0 +1,44 @@
+function worktree-add --description 'Add a new worktree'
+    set full_repo $(git remote get-url origin) || return 1
+    set dir_prefix
+    if ! set repo $(echo $full_repo | rg '^https:\/\/github\.com\/CruGlobal\/(.+?)\.git$' -r '$1')
+        echo "Repo \"$full_repo\" does not match a CruGlobal GitHub repo"
+        return 1
+    end
+
+    set dir_prefix $(dasel --file $HOME/.config/worktrees.toml "repos.$repo.prefix" --write plain || return 1)
+    set init_command $(dasel --file $HOME/.config/worktrees.toml "repos.$repo.property(init_command?)" --write plain || return 1)
+
+    set branch_type $(gum filter new existing --header="Do you want to base the worktree on a new branch or an existing branch?" --placeholder="" || return 1)
+    switch $branch_type
+        case new
+            set branch $(gum input --prompt "New branch name: " --placeholder "" || return 1)
+        case existing
+            set branch $(git for-each-ref 'refs/heads/*' 'refs/remotes/origin/*' --format '%(refname:short)' --sort '-committerdate' | fzf || return 1)
+            set branch $(echo $branch | string split /)[-1]
+    end
+
+    set dir_suffix $(gum input --prompt "Directory name: $dir_prefix-" --placeholder="" --value=$branch) || return 1
+    set directory $HOME/dev/$dir_prefix-$dir_suffix
+
+    switch $branch_type
+        case new
+            git worktree add -b $branch $directory $(git primary) || return 1
+        case existing
+            git worktree add $directory --checkout $branch || return 1
+    end
+
+    z $directory
+    if set -q init_command
+        echo $init_command | source
+    end
+    if test -e yarn.lock
+        yarn
+    else if test -e package-lock.json
+        npm install
+    else
+        echo "No yarn.lock or package-lock.json file found"
+    end
+    portman create
+    code $directory
+end

22fec1caf73504131e7d11ed74f579092af93c65
Caleb Cox <canac@users.noreply.github.com>
2024-03-14 11:47:39 -0500

    Use new fzf shell integration

--- .chezmoiscripts/run_onchange_before_phase2-fzf.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Configure fzf
-"$(brew --prefix fzf)/install" --key-bindings --no-completion --no-update-rc --no-bash --no-zsh
-# Revert the fzf installer setting the fish_user_paths universal variable
-# because we want global variables exclusively set in config.fish
-fish -c 'set --erase -U fish_user_paths' || true
--- private_dot_config/private_fish/functions/fish_user_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-function fish_user_key_bindings
-    fzf_key_bindings
-    # McFly keybinding for Ctrl+R overrides fzf keybinding
-    mcfly_key_bindings
-end

87f05ab2455508e14462e59ed501ff5bd8598143
Caleb Cox <canac@users.noreply.github.com>
2024-10-07 10:59:12 -0500

    Start tests immediately

--- private_dot_config/private_fish/functions/jr.fish
+++ private_dot_config/private_fish/functions/jr.fish
@@ -1,3 +1,5 @@
 function jr --description 'Test a file with jest'
-    set file $(fd --type file --glob '*.{spec,test}.{js,ts,tsx}' | fzf --query=$argv[1] --select-1) && commandline --replace "yarn jest --runTestsByPath $file"
+    set file $(fd --type file --glob '*.{spec,test}.{js,ts,tsx}' | fzf --query=$argv[1] --select-1) &&
+        commandline --replace "yarn jest --runTestsByPath $file" &&
+        commandline --function execute
 end
--- private_dot_config/private_fish/functions/jrw.fish
+++ private_dot_config/private_fish/functions/jrw.fish
@@ -1,3 +1,5 @@
 function jrw --description 'Test a file with jest in watch mode'
-    set file $(fd --type file --glob '*.{spec,test}.{js,ts,tsx}' | fzf --query=$argv[1] --select-1) && commandline --replace "yarn jest --runTestsByPath $file --watch"
+    set file $(fd --type file --glob '*.{spec,test}.{js,ts,tsx}' | fzf --query=$argv[1] --select-1) &&
+        commandline --replace "yarn jest --runTestsByPath $file --watch" &&
+        commandline --function execute
 end

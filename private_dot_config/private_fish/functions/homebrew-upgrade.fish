93528a02cbe1008b532486a0342f20027b7f7792
Caleb Cox <canac@users.noreply.github.com>
2024-11-25 16:11:39 -0600

    homebrew-upgrade: stop creating temporary file

--- private_dot_config/private_fish/functions/homebrew-upgrade.fish
+++ private_dot_config/private_fish/functions/homebrew-upgrade.fish
@@ -1,6 +1,3 @@
 function homebrew-upgrade --description 'Update Homebrew and upgrade all installed packages'
-    set tempfile /tmp/update-homebrew-$(date +%s).txt
-    HOMEBREW_COLOR=1 brew upgrade | tee $tempfile
-    sed -e 's/\x1b\[[0-9;]*m//g' $tempfile | rg "==> Upgrading \d+ outdated packages?:\n([\s\S]+?)\n==>" --multiline --only-matching --replace '$1' | awk '{print "homebrew-upgrade\t"$0}' | mailbox import
-    rm $tempfile
+    HOMEBREW_COLOR=1 brew upgrade | tee /dev/stdout | sed -e 's/\x1b\[[0-9;]*m//g' | rg "==> Upgrading \d+ outdated packages?:\n([\s\S]+?)\n==>" --multiline --only-matching --replace '$1' | awk '{print "homebrew-upgrade\t"$0}' | mailbox import
 end

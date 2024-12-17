b7684b8ee395b5450197b7e5643ae1845f66f815
Caleb Cox <canac@users.noreply.github.com>
2024-12-17 14:39:54 -0600

    homebrew-upgrade: fix color codes when stdout is not a TTY

--- private_dot_config/private_fish/functions/homebrew-upgrade.fish
+++ private_dot_config/private_fish/functions/homebrew-upgrade.fish
@@ -1,3 +1,3 @@
 function homebrew-upgrade --description 'Update Homebrew and upgrade all installed packages'
-    HOMEBREW_COLOR=1 brew upgrade | tee /dev/stdout | sed -e 's/\x1b\[[0-9;]*m//g' | rg "==> Upgrading \d+ outdated packages?:\n([\s\S]+?)\n==>" --multiline --only-matching --replace '$1' | awk '{print "homebrew-upgrade\t"$0}' | mailbox import
+    brew upgrade | tee /dev/stdout | sed -e 's/\x1b\[[0-9;]*m//g' | rg "==> Upgrading \d+ outdated packages?:\n([\s\S]+?)\n==>" --multiline --only-matching --replace '$1' | awk '{print "homebrew-upgrade\t"$0}' | mailbox import
 end

b9087c10482d96395be2d06aba45ac3bef5ebf0a
Caleb Cox <canac@users.noreply.github.com>
2025-01-09 15:22:56 -0600

    worktree-add: accept branch from argument

--- private_dot_config/private_fish/functions/worktree-add.fish
+++ private_dot_config/private_fish/functions/worktree-add.fish
@@ -1,6 +1,6 @@
 function worktree-add --description 'Add a new worktree'
-    set full_repo $(git remote get-url origin) || return 1
-    if ! set repo $(echo $full_repo | rg '^https:\/\/github\.com\/CruGlobal\/(.+?)\.git$' -r '$1')
+    set full_repo $(git remote get-url origin || return 1)
+    if not set repo $(echo $full_repo | rg '^https:\/\/github\.com\/CruGlobal\/(.+?)\.git$' -r '$1')
         echo "Repo \"$full_repo\" does not match a CruGlobal GitHub repo"
         return 1
     end
@@ -8,17 +8,29 @@ function worktree-add --description 'Add a new worktree'
     set dir_prefix $(dasel --file $HOME/.config/worktrees.toml "repos.$repo.prefix" --write plain || return 1)
     set init_command $(dasel --file $HOME/.config/worktrees.toml "repos.$repo.property(init_command?)" --write plain || return 1)
 
-    set branch_type $(gum filter new existing --header="Do you want to base the worktree on a new branch or an existing branch?" --placeholder="" || return 1)
-    git fetch
-    switch $branch_type
-        case new
-            set branch $(gum input --prompt="New branch name: " --placeholder="" --width=0 || return 1)
-        case existing
-            set branch $(git for-each-ref 'refs/heads/*' 'refs/remotes/origin/*' --format '%(refname:short)' --sort '-committerdate' | fzf || return 1)
-            set branch $(echo $branch | string split /)[-1]
+    # Use the branch name from the first argument if present
+    if test $(count $argv) -ge 1
+        set branch $argv[1]
+        if git show-ref --quiet refs/heads/$branch
+            set branch_type existing
+        else
+            set branch_type new
+        end
+        echo "Using $branch_type branch $branch"
     end
 
-    set dir_suffix $(gum input --prompt="Directory name: $dir_prefix-" --placeholder="" --width=0 --value=$branch) || return 1
+    if not set --query branch_type
+        set branch_type $(gum filter new existing --header="Do you want to base the worktree on a new branch or an existing branch?" --height=3 --placeholder="" || return 1)
+        switch $branch_type
+            case new
+                set branch $(gum input --prompt="New branch name: " --placeholder="" || return 1)
+            case existing
+                set branch $(git for-each-ref 'refs/heads/*' 'refs/remotes/origin/*' --format '%(refname:short)' --sort '-committerdate' | fzf || return 1)
+                set branch $(echo $branch | string split /)[-1]
+        end
+    end
+
+    set dir_suffix $(gum input --prompt="Directory name: $dir_prefix-" --placeholder="" --value=$branch || return 1)
     set directory $HOME/dev/$dir_prefix-$dir_suffix
 
     switch $branch_type

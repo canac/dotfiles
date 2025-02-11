14e4be21bd531e7d0efdcdc46c8678a537d3c5b3
Caleb Cox <canac@users.noreply.github.com>
2025-02-11 15:56:42 -0600

    dns-resolver: add none option

---
+++ dev/exact_raycast-scripts/executable_dns-none.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Disable DNS filtering
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🚫
+
+# Documentation:
+# @raycast.description Start using the network default for DNS resolution on this machine
+# @raycast.author Caleb Cox
+# @raycast.authorURL https://github.com/canac
+
+dns-resolver none
+echo "Using network default for DNS resolution"

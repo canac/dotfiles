2dfa500c0a352c18b6969841f61da37aab99a03a
Caleb Cox <canac@users.noreply.github.com>
2025-02-20 16:51:50 -0600

    postgres: upgrade to version 17

--- .chezmoiscripts/run_onchange_before_phase2-postgres.sh
+++ .chezmoiscripts/run_onchange_before_phase2-postgres.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+brew services restart postgresql@17
+
 # Delete the Homebrew-created database and create a new one with username postgres
-pg_bin=$(brew --prefix postgresql@15)/bin
+pg_bin=$(brew --prefix postgresql@17)/bin
 if "$pg_bin/psql" -c "\du" postgres | grep --silent "$USER"; then
-  data_dir=$(brew --prefix)/var/postgresql@15
+  data_dir=$(brew --prefix)/var/postgresql@17
   rm -rf "$data_dir"
   "$pg_bin/initdb" --locale=C --encoding=UTF-8 --username=postgres "$data_dir"
 fi
-
-brew services restart postgresql@15

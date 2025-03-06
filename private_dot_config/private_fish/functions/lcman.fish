d943c6f55ea99e85637542daea41a8418a12d46c
Caleb Cox <canac@users.noreply.github.com>
2025-03-06 08:59:33 -0600

    lcman: add print and support other namespaces

--- private_dot_config/private_fish/functions/lcman.fish
+++ private_dot_config/private_fish/functions/lcman.fish
@@ -1,24 +1,34 @@
 function lcman --description 'launchctl manager'
+    function usage
+        echo "Usage: lcman [print|start|stop|restart] [service]"
+    end
+
     if test $(count $argv) -ne 2
-        echo "Usage: lcman [start|stop|restart] [service]"
+        usage
         return 1
     end
 
     set command $argv[1]
     set service $argv[2]
     set domain_target gui/$(id -u $USER)
-    set plist_file $HOME/Library/LaunchAgents/com.canac.$service.plist
+    if not set plist_file $(fd --type file . $HOME/Library/LaunchAgents | rg --fixed-strings ".$service.plist")
+        echo "Service not found: $service"
+        return 1
+    end
+    set full_service $(basename $plist_file .plist) || return 1
 
     switch $command
+        case print
+            bat $plist_file
         case start
             launchctl bootstrap $domain_target $plist_file
         case stop
-            launchctl bootout $domain_target/com.canac.$service
+            launchctl bootout $domain_target/$full_service
         case restart
-            launchctl bootout $domain_target/com.canac.$service
+            launchctl bootout $domain_target/$full_service
             launchctl bootstrap $domain_target $plist_file
         case '*'
-            echo "Invalid command: $command must be one of start, stop, restart"
+            usage
             return 1
     end
 end

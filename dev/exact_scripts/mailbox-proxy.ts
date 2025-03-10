d104409ef99258d28f37b57e5ee1f6d5427c81d6
Caleb Cox <canac@users.noreply.github.com>
2025-03-10 10:06:05 -0500

    git-scripts: rename directory to bin

--- dev/exact_scripts/mailbox-proxy.ts
+++ dev/exact_scripts/mailbox-proxy.ts
@@ -8,16 +8,28 @@ using kv = await Deno.openKv(
   `https://api.deno.com/databases/${databaseId}/connect`,
 );
 
-for await (const _ of kv.watch([["lastId"]])) {
-  const messageEntries = await Array.fromAsync(kv.list({
-    prefix: ["messages"],
-  }));
-  const messages = messageEntries.flatMap(
-    ({ value }) =>
-      typeof value === "string"
-        ? [{ mailbox: "mailbox-proxy", content: value }]
-        : [],
-  );
-  await createMailboxMessages(messages);
-  await Promise.all(messageEntries.map(({ key }) => kv.delete(key)));
+try {
+  for await (const _ of kv.watch([["lastId"]])) {
+    const messageEntries = await Array.fromAsync(kv.list({
+      prefix: ["messages"],
+    })).catch((err) => {
+      console.log(new Date().toISOString(), "List error", err);
+      throw err;
+    });
+    const messages = messageEntries.flatMap(
+      ({ value }) =>
+        typeof value === "string"
+          ? [{ mailbox: "mailbox-proxy", content: value }]
+          : [],
+    );
+    await createMailboxMessages(messages);
+    await Promise.all(messageEntries.map(({ key }) => kv.delete(key))).catch(
+      (err) => {
+        console.log(new Date().toISOString(), "Delete error", err);
+        throw err;
+      },
+    );
+  }
+} catch (err) {
+  console.log(new Date().toISOString(), "Watch error", err);
 }

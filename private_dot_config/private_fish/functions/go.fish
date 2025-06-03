3ba6fe50602dcd2869e24a5c3d14996315efc61f
Caleb Cox <canac@users.noreply.github.com>
2025-06-03 11:24:15 -0500

    go: fix typo

--- private_dot_config/private_fish/functions/go.fish
+++ private_dot_config/private_fish/functions/go.fish
@@ -1,3 +1,3 @@
-function go --description 'Change the currenct directory to a development project'
+function go --description 'Change the current directory to a development project'
     set dir $(fd . ~/dev --type=directory --maxdepth=1 | fzf) && z $dir
 end

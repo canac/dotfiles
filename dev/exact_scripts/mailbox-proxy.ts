cae13cda763296b2908f388b04b15fc073c9ebca
Caleb Cox <canac@users.noreply.github.com>
2025-07-01 10:23:41 -0500

    mailbox-proxy: use websockets

--- dev/exact_scripts/mailbox-proxy.ts
+++ dev/exact_scripts/mailbox-proxy.ts
@@ -1,35 +1,24 @@
 import { createMailboxMessages } from "./mailbox.ts";
 
-const databaseId = Deno.env.get("MAILBOX_PROXY_DATABASE_ID");
-if (!databaseId) {
-  throw new Error("$MAILBOX_PROXY_DATABASE_ID environment variable is not set");
-}
-using kv = await Deno.openKv(
-  `https://api.deno.com/databases/${databaseId}/connect`,
-);
+const connect = () => {
+  const ws = new WebSocket("wss://mailbox-proxy.deno.dev/");
+  ws.addEventListener("message", async (event) => {
+    if (typeof event.data === "string") {
+      await createMailboxMessages([{
+        mailbox: "mailbox-proxy",
+        content: event.data,
+      }]);
+    }
+  });
 
-try {
-  for await (const _ of kv.watch([["lastId"]])) {
-    const messageEntries = await Array.fromAsync(kv.list({
-      prefix: ["messages"],
-    })).catch((err) => {
-      console.log(new Date().toISOString(), "List error", err);
-      throw err;
-    });
-    const messages = messageEntries.flatMap(
-      ({ value }) =>
-        typeof value === "string"
-          ? [{ mailbox: "mailbox-proxy", content: value }]
-          : [],
-    );
-    await createMailboxMessages(messages);
-    await Promise.all(messageEntries.map(({ key }) => kv.delete(key))).catch(
-      (err) => {
-        console.log(new Date().toISOString(), "Delete error", err);
-        throw err;
-      },
-    );
-  }
-} catch (err) {
-  console.log(new Date().toISOString(), "Watch error", err);
-}
+  ws.addEventListener("close", () => {
+    console.log("Reconnecting to websocket...");
+    connect();
+  });
+
+  ws.addEventListener("error", () => {
+    ws.close();
+  });
+};
+
+connect();

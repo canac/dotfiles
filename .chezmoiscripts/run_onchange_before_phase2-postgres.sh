523a493c6d412f5b9270d6bfa9ba095cd9942df0
Caleb Cox <canac@users.noreply.github.com>
2025-09-25 10:58:44 -0500

    postgres: upgrade to v18

--- .chezmoiscripts/run_onchange_before_phase2-postgres.sh
+++ .chezmoiscripts/run_onchange_before_phase2-postgres.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-brew services restart postgresql@17
+brew services restart postgresql@18
 
 # Delete the Homebrew-created database and create a new one with username postgres
-pg_bin=$(brew --prefix postgresql@17)/bin
+pg_bin=$(brew --prefix postgresql@18)/bin
 if "$pg_bin/psql" -c "\du" postgres | grep --silent "$USER"; then
-  data_dir=$(brew --prefix)/var/postgresql@17
+  data_dir=$(brew --prefix)/var/postgresql@18
   rm -rf "$data_dir"
   "$pg_bin/initdb" --locale=C --encoding=UTF-8 --username=postgres "$data_dir"
 fi

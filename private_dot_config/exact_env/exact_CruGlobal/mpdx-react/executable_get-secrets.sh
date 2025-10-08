a34bcf06450fa96be330dfce73cf5ce7a719c0f2
Caleb Cox <canac@users.noreply.github.com>
2025-10-08 11:22:54 -0500

    sync-env: only get essential mpdx-react secrets

--- private_dot_config/exact_env/exact_CruGlobal/mpdx-react/executable_get-secrets.sh
+++ private_dot_config/exact_env/exact_CruGlobal/mpdx-react/executable_get-secrets.sh
@@ -2,4 +2,27 @@
 
 export AWS_PROFILE=mpdx-amplify
 (aws sts get-caller-identity || aws sso login) > /dev/null 2>&1 &&
-aws amplify get-app --app-id d3dytjb8adxkk5 | jq --raw-output '.app.environmentVariables | to_entries | .[] | "\(.key)=\(.value)"'
+aws amplify get-app --app-id d3dytjb8adxkk5 | jq --raw-output '
+  .app.environmentVariables |
+  to_entries |
+  .[] |
+  select(.key | IN(
+    "CROWDIN_API_TOKEN",
+    "GOOGLE_MAPS_API_KEY",
+    "HELPJUICE_CATEGORY_ID",
+    "HELPJUICE_KNOWLEDGE_BASE_URL",
+    "HELPJUICE_ORIGIN",
+    "HELPJUICE_PARENT_CATEGORY",
+    "HELPJUICE_SUBDOMAIN",
+    "HELP_URL_COACHING_ACTIVITY",
+    "HELP_URL_COACHING_APPOINTMENTS_AND_RESULTS",
+    "HELP_URL_SETUP_FIND_ORGANIZATION",
+    "HELP_WHATS_NEW_IMAGE_URL",
+    "HELP_WHATS_NEW_URL",
+    "NEXT_PUBLIC_MEDIA_FAVICON",
+    "NEXT_PUBLIC_MEDIA_LOGO",
+    "OKTA_CLIENT_SECRET",
+    "TERMS_OF_USE_URL"
+  )) |
+  "\(.key)=\(.value)"
+'

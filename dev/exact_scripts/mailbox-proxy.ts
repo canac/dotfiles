96ded22172be5461269ab6210eddfb1416bdea96
Caleb Cox <canac@users.noreply.github.com>
2025-10-17 13:49:35 -0500

    mailbox-proxy: authenticate

--- dev/exact_scripts/mailbox-proxy.ts
+++ dev/exact_scripts/mailbox-proxy.ts
@@ -1,7 +1,17 @@
 import { createMailboxMessages } from "./mailbox.ts";
 
+const authToken = Deno.args[0];
+if (!authToken) {
+  console.error("Usage: mailbox-proxy [auth token]");
+  Deno.exit(1);
+}
+
 const connect = () => {
-  const ws = new WebSocket("wss://mailbox-proxy.deno.dev/");
+  const ws = new WebSocket("wss://mailbox-proxy.canac.deno.net/", {
+    headers: {
+      Authorization: authToken,
+    },
+  });
   ws.addEventListener("message", async (event) => {
     if (typeof event.data === "string") {
       await createMailboxMessages([{

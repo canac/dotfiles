10324322fa253529b5e704265be8c98b95f8b387
Caleb Cox <canac@users.noreply.github.com>
2025-11-18 15:01:13 -0600

    mpdx-api: remove SAA_URL

--- private_dot_config/exact_env/exact_CruGlobal/mpdx_api/executable_get-secrets.sh
+++ private_dot_config/exact_env/exact_CruGlobal/mpdx_api/executable_get-secrets.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cru application secrets read --name mpdx_api --keys OKTA_AUTH_CLIENT_IDS,OKTA_ISSUER,OKTA_SERVER_PATH,OKTA_SERVER_URL,SAA_URL,SAA_UID,SAA_SECRET
+cru application secrets read --name mpdx_api --keys OKTA_AUTH_CLIENT_IDS,OKTA_ISSUER,OKTA_SERVER_PATH,OKTA_SERVER_URL,SAA_UID,SAA_SECRET

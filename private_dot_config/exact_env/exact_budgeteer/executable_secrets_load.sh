b6a45558228ad47722ff98134bc67c3369f6be08
Caleb Cox <canac@users.noreply.github.com>
2026-02-09 11:18:36 -0600

    budgeteer: speed up passphrase generation

--- private_dot_config/exact_env/exact_budgeteer/executable_secrets_load.sh
+++ private_dot_config/exact_env/exact_budgeteer/executable_secrets_load.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "VITE_JWT_SECRET=$(openssl rand -hex 32)"
-echo "VITE_AUTH_PASSWORD=$(bw generate --passphrase | cut -d'-' -f1)"
+echo "VITE_AUTH_PASSWORD=$(curl -s https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt | sort -R | head -n1 | cut -f2)"

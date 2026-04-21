c4e72391fb089798837aed532811f8fd361ff510
Caleb Cox <canac@users.noreply.github.com>
2026-04-20 20:26:45 -0500

    budgeteer: correct env names

--- private_dot_config/exact_env/exact_budgeteer/executable_secrets_load.sh
+++ private_dot_config/exact_env/exact_budgeteer/executable_secrets_load.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo "VITE_JWT_SECRET=$(openssl rand -hex 32)"
-echo "VITE_AUTH_PASSWORD=$(curl -s https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt | sort -R | head -n1 | cut -f2)"
+echo "AUTH_PASSWORD=$(curl -s https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt | sort -R | head -n1 | cut -f2)"
+echo "JWT_SECRET=$(openssl rand -hex 32)"

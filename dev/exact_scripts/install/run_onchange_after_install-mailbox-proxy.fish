0842c13485950f435534a6155817f9462e88a72f
Caleb Cox <canac@users.noreply.github.com>
2026-05-06 08:50:50 -0500

    mailbox-proxy: remove mailbox-proxy

--- dev/exact_scripts/install/run_onchange_after_install-mailbox-proxy.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env fish
-
-# Install mailbox-proxy
-deno install --global --force \
-    --allow-env=PORT \
-    --allow-net=0.0.0.0 \
-    --allow-run=mailbox \
-    ~/dev/scripts/mailbox-proxy.ts

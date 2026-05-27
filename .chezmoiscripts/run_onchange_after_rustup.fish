2680b295477aee164e3cfb0202a44407cec9392e
Caleb Cox <canac@users.noreply.github.com>
2026-05-27 15:56:59 -0500

    rustup: install toolchain

--- .chezmoiscripts/run_onchange_after_rustup.fish
+++ .chezmoiscripts/run_onchange_after_rustup.fish
@@ -2,11 +2,11 @@
 
 # Run rustup-init if it hasn't been run before
 if not test -e ~/.rustup
-    rustup-init -y --no-modify-path
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 end
 
 # Install clippy if it isn't installed
-set rustup_bin ~/.cargo/bin/rustup
-if not $rustup_bin component list --installed | grep -q clippy
-    $rustup_bin component add clippy
-end
+rustup component add clippy
+
+# Install the stable toolchain
+rustup install

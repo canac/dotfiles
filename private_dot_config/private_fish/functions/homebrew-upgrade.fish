8c76467aea9b356a6e6ffb485be6677e2348004b
Caleb Cox <canac@users.noreply.github.com>
2026-06-04 10:07:44 -0500

    homebrew-upgrade: fix duplicate messages

--- private_dot_config/private_fish/functions/homebrew-upgrade.fish
+++ private_dot_config/private_fish/functions/homebrew-upgrade.fish
@@ -1,3 +1,8 @@
 function homebrew-upgrade --description 'Update Homebrew and upgrade all installed packages'
-    brew upgrade | tee /dev/stdout | sed -e 's/\x1b\[[0-9;]*m//g' | rg "==> Upgrading \d+ outdated packages?:\n([\s\S]+?)\n==>" --multiline --only-matching --replace '$1' | awk '{print "homebrew-upgrade\t"$0}' | mailbox import
+    set --local fifo (mktemp -u)
+    mkfifo $fifo
+    brew upgrade | tee $fifo &
+    sed -e 's/\x1b\[[0-9;]*m//g' <$fifo | rg "==> Upgrading \d+ outdated packages?:\n([\s\S]+?)\n==>" --multiline --only-matching --replace '$1' | awk '{print "homebrew-upgrade\t"$0}' | mailbox import
+    wait
+    rm $fifo
 end
